e7733a35009cfed610ff987ec61f128466f0d88d
caokangkang <hkd_ckk@163.com>
2023-04-13 18:06:29 +0800

    修改

---
+++ LeetCode/medium/两数相加/index.js
@@ -0,0 +1,10 @@
+function ListNode() {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+function addTwoNumbers(l1, l2) {
+  let dummy = new ListNode();
+  console.log('dummy :>> ', dummy);
+}
+
+console.log(new ListNode())
